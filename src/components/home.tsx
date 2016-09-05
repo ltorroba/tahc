702813d4969c4080dc9b2177930773c9050e9206
Lucas Federico Torroba Hennigen <lucasfth@gmail.com>
2016-09-05 18:19:58 -0300

    feat(home): add home text
    Add home text
    Add home styles
    Fix page default font

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -1,9 +1,17 @@
 import * as React from "react";
 
+import "../stylesheets/home";
+
 export class Home extends React.Component<{}, {}> {
   render() {
     return (
-      <p>Hello, world!</p>
+      <div className="home">
+        <div>
+          <p className="figure">^_^</p>
+          <p className="label">Start chatting by clicking a chat on the left</p>
+          <p className="label">Or navigate to www.tahc.pw/chatname</p>
+        </div>
+      </div>
     );
   }
 }
