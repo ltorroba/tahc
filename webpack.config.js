bcddb83d2a4d7ec5ad7a716388b5f7b4a2437f5c
Lucas Federico Torroba Hennigen <lucasfth@gmail.com>
2016-09-05 01:12:28 -0300

    feat(webpack): enable hot module replacement

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,29 @@
+var webpack = require('webpack');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: "./src/index.tsx",
+  entry: [
+    'webpack-dev-server/client?http://0.0.0.0:8080',
+    'webpack/hot/only-dev-server',
+    "./src/index.tsx"
+  ],
+
   output: {
     filename: "./dist/bundle.js",
   },
 
-  // Enable sourcemaps for debugging webpack's output.
   devtool: "source-map",
 
   resolve: {
-    // Add '.ts' and '.tsx' as resolvable extensions.
     extensions: ["", ".webpack.js", ".web.js", ".ts", ".tsx", ".js"]
   },
 
   module: {
     loaders: [
-      // All files with a '.ts' or '.tsx' extension will be handled by 'ts-loader'.
-      { test: /\.tsx?$/, loader: "ts-loader" }
+      { test: /\.tsx?$/, loaders: ["react-hot-loader/webpack", "ts-loader"] }
     ],
 
     preLoaders: [
-      // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
       { test: /\.js$/, loader: "source-map-loader" }
     ]
   },
